44a56296859a321ac8b348ceebea5c4fa493c3a4
yu03040 <131416689+yu03040@users.noreply.github.com>
2024-06-08 15:52:48 +0900

    Fix：ブループリントで書いていた部分をC++に修正
    ・ゲームクリア条件
    ・制限時間のUIの表示
    
    # GUNMAN

--- GUNMAN/Source/GUNMAN/GUNMANGameMode.cpp
+++ GUNMAN/Source/GUNMAN/GUNMANGameMode.cpp
@@ -6,7 +6,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "Runtime/Engine/Public/TimerManager.h"
-#include "UMG/UITitle.h"
+#include "UMG/UITimeLimitWidget.h"
 #include "UObject/ConstructorHelpers.h"
 
 AGUNMANGameMode::AGUNMANGameMode() : Super()
@@ -19,6 +19,8 @@ AGUNMANGameMode::AGUNMANGameMode() : Super()
 	}
 
 	PlayerControllerClass = AGUNMANController::StaticClass();
+
+	GameClearKillCount = 10;
 }
 
 void AGUNMANGameMode::Tick(float DeltaTime)
@@ -33,7 +35,8 @@ void AGUNMANGameMode::Tick(float DeltaTime)
 		UGameplayStatics::OpenLevel(this, "GameOverMap");
 	}
 
-	if (PlayerRef->GetKillCount() >= 25)
+	// 1敵を10体以上倒したらゲームクリア
+	if (PlayerRef->GetKillCount() >= GameClearKillCount)
 	{
 		FTimerHandle TimerHandle;
 		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AGUNMANGameMode::OpenGameClearMap, 2.0f);
@@ -44,10 +47,30 @@ void AGUNMANGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// プレイヤ―の情報をもらう
 	auto IsPlayer = IsValid(UGameplayStatics::GetPlayerCharacter(this->GetWorld(), 0));
 	if (IsPlayer == true)
 	{
 		PlayerRef = Cast<AGUNMANCharacter>(UGameplayStatics::GetPlayerCharacter(this->GetWorld(), 0));
+	}
+
+	// プレイヤーコントローラーを取得
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+
+	// ウィジェットブループリントのパスをセット
+	FString path = "/Game/UMG/WBP_TimeLimit.WBP_TimeLimit_C";
+	// アセットパスから UserWidgetClass を生成する
+	WidgetClass = TSoftClassPtr<UUserWidget>(FSoftObjectPath(*path)).LoadSynchronous();
+	// タイトルのウィジェットを作成
+	if (IsValid(WidgetClass))
+	{
+
+		UITimeLimitRef = Cast<UUITimeLimitWidget>(CreateWidget(PlayerController, WidgetClass));
+
+		if (UITimeLimitRef)
+		{
+			UITimeLimitRef->AddToViewport();
+		}
 	}
 }
 
