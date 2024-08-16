1aaa5ace6abe706d614db35eb01c920fa16c21cc
yu03040 <kazumbl1no@gmail.com>
2024-08-16 13:44:31 +0900

    Fix：Blueprint の処理を C++ に移動

---
+++ GUNMAN/Source/GUNMAN/GameModes/GameClearMode.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GUNMAN/GameModes/GameClearMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "GUNMAN/UMG/UIGameClear.h"
+
+void AGameClearMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DisplayGameClear();
+}
+
+void AGameClearMode::DisplayGameClear()
+{
+	// WidgetBlueprint の Class を取得する
+	FString Path = TEXT("/Game/UMG/WBP_GameClear.WBP_GameClear_C");
+	TSubclassOf<UUserWidget> WidgetClass = TSoftClassPtr<UUserWidget>(FSoftObjectPath(*Path)).LoadSynchronous();
+
+	// PlayerController を取得する
+	TObjectPtr<APlayerController> PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+
+	// WidgetClass と PlayerController が取得できたか判定する
+	if (WidgetClass && PlayerController)
+	{
+		// クリア用のウィジェットを作成する
+		TObjectPtr<UUIGameClear> RunScoreWidget = CreateWidget<UUIGameClear>(PlayerController, WidgetClass);
+		if (RunScoreWidget)
+		{
+			// クリアを画面に表示する
+			RunScoreWidget->AddToViewport();
+		}
+	}
+}
