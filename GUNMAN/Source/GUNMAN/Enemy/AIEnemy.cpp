9572c886b6f39a7a43533443394fce914167670f
yu03040 <kazumbl1no@gmail.com>
2024-08-20 00:09:34 +0900

    Fix：敵(AIEnemy) と AI コントローラー(AIEnemyController) を修正
    ・AIPerception を AIEnemy から AIEnemyController に移動（AIPerceptionComponent は AAIController クラスと一緒に使用されるべきであるという警告が出るため）
    ・AIEnemy を継承したブループリントクラスの処理を AIEnemy に記述

--- GUNMAN/Source/GUNMAN/Enemy/AIEnemy.cpp
+++ GUNMAN/Source/GUNMAN/Enemy/AIEnemy.cpp
@@ -4,20 +4,22 @@
 #include "AIEnemy.h"
 #include "AIEnemyController.h"
 #include "GUNMAN/UMG/UIEnemy.h"
+#include "GUNMAN/ArmedWeapon/WeaponStructure.h"
 #include "Perception/AIPerceptionComponent.h"
+#include "Engine/DataTable.h"
 #include "Kismet/GamePlayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Kismet/KismetStringLibrary.h"
 #include "Components/CapsuleComponent.h"
-#include "GameFramework/CharacterMovementComponent.h"
-#include "Perception/AISenseConfig_Sight.h" // 視覚のセンサータイプ
 
 // Sets default values
 AAIEnemy::AAIEnemy()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	AIControllerClass = AAIEnemyController::StaticClass();
 
 	// Weapon を Mesh の子に
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
@@ -27,12 +29,36 @@ AAIEnemy::AAIEnemy()
 	Widget = CreateDefaultSubobject<UWidgetComponent>(TEXT("Widget"));
 	Widget->SetupAttachment(GetMesh());
 
-	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
-
 	// スポーンされた敵に対してもAIを適用
-	AutoPossessAI =  EAutoPossessAI::PlacedInWorldOrSpawned;
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 
-	IsAlive = true;
+	// DataTable をロード
+	static ConstructorHelpers::FObjectFinder<UDataTable> DataTableFinder(TEXT("/Game/GUNMAN/Blueprint/ArmedWeapon/DT_Weapon.DT_Weapon"));
+	if (DataTableFinder.Succeeded())
+	{
+		WeaponDataTable = DataTableFinder.Object;
+	}
+
+	// カーブアセットの取得
+	const ConstructorHelpers::FObjectFinder<UCurveFloat> Find(TEXT("CurveFloat'/Game/GUNMAN/Blueprint/Enemy/CB_EnemyRun.CB_EnemyRun'"));
+	if (Find.Succeeded())
+	{
+		FloatCurve = Find.Object;
+	}
+
+	// タイムライン初期化
+	RunTimeline = new FTimeline();
+
+	// タイムラインモード設定
+	RunTimeline->SetTimelineLengthMode(ETimelineLengthMode::TL_TimelineLength);
+	RunTimeline->SetTimelineLength(1.0f);
+
+	// タイムライン更新時のデリゲート
+	FOnTimelineFloat TimeLineFloat;
+	TimeLineFloat.BindUFunction(this, TEXT("TimeLineFunc"));
+	RunTimeline->AddInterpFloat(FloatCurve, TimeLineFloat);
+
+	bIsAlive = true;
 	CurrentHealth = 30.0f;
 	MaxHealth = CurrentHealth;
 	DeadHealth = 0.0f;
@@ -60,12 +86,111 @@ void AAIEnemy::BeginPlay()
 		EnemyWidget = WidgetClass;
 		EnemyWidget->SetOwningEnemy(this);
 	}
+
+	FName RowName = FName(TEXT("Rifle"));
+	FWeaponStructure* Row = WeaponDataTable->FindRow<FWeaponStructure>(RowName, "");
+	if (Row)
+	{
+		// 武器をメッシュにアタッチ
+		Weapon->AttachToComponent(
+			GetMesh(),
+			FAttachmentTransformRules(
+				EAttachmentRule::KeepRelative,
+				EAttachmentRule::KeepRelative,
+				EAttachmentRule::KeepRelative,
+				true),
+			Row->EquipSocketName
+		);
+	}
 }
 
 // Called every frame
 void AAIEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AAIEnemy::AttackCharacter_Implementation()
+{
+	if (bIsAlive)
+	{
+		FName RowName = FName(TEXT("Rifle"));
+		FWeaponStructure* Row = WeaponDataTable->FindRow<FWeaponStructure>(RowName, "");
+		if (Row)
+		{
+			EquippedWeaponInformation.GunshotSound = Row->GunshotSound;
+			EquippedWeaponInformation.MuzzleFire = Row->MuzzleFire;
+			EquippedWeaponInformation.MuzzleFireSoketName = Row->MuzzleFireSoketName;
+			EquippedWeaponInformation.FiringMontage = Row->FiringMontage;
+			EquippedWeaponInformation.AmmunitionClass = Row->AmmunitionClass;
+			EquippedWeaponInformation.AmmunitionSocketName = Row->AmmunitionSocketName;
+
+			// 効果音再生
+			UGameplayStatics::PlaySound2D(this, EquippedWeaponInformation.GunshotSound);
+
+			// アニメーション再生
+			PlayAnimMontage(EquippedWeaponInformation.FiringMontage, 0.1);
+
+			// 銃口エフェクトを発生
+			UGameplayStatics::SpawnEmitterAttached(
+				EquippedWeaponInformation.MuzzleFire,
+				Weapon,
+				EquippedWeaponInformation.MuzzleFireSoketName,
+				FVector(0.0f, 0.0f, 0.0f),
+				FRotator(0.0f, 0.0f, 0.0f),
+				FVector(1.0f, 1.0f, 1.0f),
+				EAttachLocation::KeepRelativeOffset,
+				true,
+				EPSCPoolMethod::None,
+				true
+			);
+
+			// 弾薬生成
+			CreateAmmunition();
+		}
+	}
+}
+
+void AAIEnemy::CreateAmmunition()
+{
+	FTransform SpawnTransform = Weapon->GetSocketTransform(
+		EquippedWeaponInformation.MuzzleFireSoketName,
+		ERelativeTransformSpace::RTS_World
+	);
+
+	// 弾薬が銃口から飛ぶ
+	TObjectPtr<AActor> SpawnActor = GetWorld()->SpawnActor<AActor>(
+		EquippedWeaponInformation.AmmunitionClass,
+		SpawnTransform
+	);
+
+	FVector Start = Weapon->GetSocketLocation(EquippedWeaponInformation.AmmunitionSocketName);
+	FVector End = Start + 10000.0f * UKismetMathLibrary::GetForwardVector(Weapon->GetSocketRotation(EquippedWeaponInformation.MuzzleFireSoketName));
+	TArray<TObjectPtr<AActor>> ActorsToIgnore;
+	FHitResult OutHit;
+
+	// レーザー光を飛ばす
+	UKismetSystemLibrary::LineTraceSingle(
+		this,
+		Start,
+		End,
+		UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_Visibility),
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		OutHit,
+		true,
+		FLinearColor::Red,
+		FLinearColor::Green,
+		0.1f
+	);
+
+	// プレイヤーに当たったら
+	if (OutHit.GetActor() == Player)
+	{
+		// ダメージを与える
+		UGameplayStatics::ApplyDamage(Player, EnemyATK, nullptr, nullptr, nullptr);
+	}
 }
 
 void AAIEnemy::HandleAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
@@ -86,7 +211,7 @@ void AAIEnemy::HandleAnyDamage(AActor* DamagedActor, float Damage, const UDamage
 			GetMesh()->SetSimulatePhysics(true);
 			// 指定された秒数が過ぎると崩れる
 			SetLifeSpan(InLifeSpan);
-			IsAlive = false;
+			bIsAlive = false;
 		}
 	}
 	else
@@ -97,27 +222,33 @@ void AAIEnemy::HandleAnyDamage(AActor* DamagedActor, float Damage, const UDamage
 	}
 }
 
-
-void AAIEnemy::TargetLost()
-{
-	TObjectPtr<AAIEnemyController> AIEnemyController = CastChecked<AAIEnemyController>(EnemyController);
-	if (AIEnemyController)
-	{
-		AIEnemyController->UpdateTargetActorKey_Implementation(NULL);
-	}
-}
-
-float AAIEnemy::GetCurrentHealth()
+float AAIEnemy::GetCurrentHealth() const
 {
 	return CurrentHealth;
 }
 
-float AAIEnemy::GetMaxHealth()
-{
-	return MaxHealth;
-}
-
-float AAIEnemy::GetHealthPercent()
+float AAIEnemy::GetHealthPercent() const
 {
 	return CurrentHealth / MaxHealth;
+}
+
+void AAIEnemy::TimeLineFunc(float Value)
+{
+	// 走るスピードを滑らかに
+	GetCharacterMovement()->MaxWalkSpeed = UKismetMathLibrary::FInterpTo(
+		GetCharacterMovement()->MaxWalkSpeed,
+		EnemySpeed,
+		UGameplayStatics::GetWorldDeltaSeconds(this),
+		5.0f
+	);
+}
+
+void AAIEnemy::ChangeRunningSpeed_Implementation(float Speed)
+{
+	// タイムライン開始
+	if (RunTimeline)
+	{
+		EnemySpeed = Speed;
+		RunTimeline->PlayFromStart();
+	}
 }
--- GUNMAN/Source/GUNMAN/Enemy/AIEnemy.h
+++ GUNMAN/Source/GUNMAN/Enemy/AIEnemy.h
@@ -4,23 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Components/WidgetComponent.h"
+#include "Components/TimelineComponent.h"
 #include "Perception/AIPerceptionComponent.h"
+#include "GUNMAN/Enemy/AIEnemyInterface.h"
+#include "GUNMAN/Enemy/EnemyAIControllerInterface.h"
 #include "GUNMAN/UMG/UIEnemy.h"
+#include "GUNMAN/ArmedWeapon/WeaponStructure.h"
 #include "AIEnemy.generated.h"
 
+class UTimelineComponent;
+class UCurveFloat;
+
 UCLASS()
-class GUNMAN_API AAIEnemy : public ACharacter
+class GUNMAN_API AAIEnemy : public ACharacter, public IAIEnemyInterface, public IEnemyAIControllerInterface
 {
 	GENERATED_BODY()
 
+private:
 	/** ThirdPerson の武器 */
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Enemy, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<USkeletalMeshComponent> Weapon;
-
-	/** 敵が意思決定をするめための変数 */
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Enemy, meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UAIPerceptionComponent> AIPerception;
 
 	/** Healthバーを表示するための変数 */
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Enemy, meta = (AllowPrivateAccess = "true"))
@@ -46,46 +51,28 @@ class GUNMAN_API AAIEnemy : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Enemy, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UUIEnemy> EnemyWidget;
 
-public:
-	// Sets default values for this character's properties
-	AAIEnemy();
+	UPROPERTY(EditAnywhere, Category = "Data")
+	TObjectPtr<UDataTable> WeaponDataTable;
 
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	FWeaponStructure EquippedWeaponInformation;
 
-	/* 現在の敵の体力のgetter */
-	UFUNCTION(BlueprintCallable, Category = Enemy)
-	float GetCurrentHealth();
+	/* カーブ */
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UCurveFloat> FloatCurve;
 
-	/* 敵の体力のgetter */
-	UFUNCTION(BlueprintCallable, Category = Enemy)
-	float GetMaxHealth();
+	/* タイムライン */
+	FTimeline* RunTimeline;
 
-	/* 敵の体力の割合を返す関数 */
-	UFUNCTION(BlueprintCallable, Category = Enemy)
-	float GetHealthPercent();
+	float EnemySpeed;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	/* 被ダメージ処理関数 */ 
-	UFUNCTION()
-	void HandleAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
-
-	/** Set Timer by Event のイベント */
-	UFUNCTION(BlueprintCallable, Category = Enemy)
-	void TargetLost();
-
-protected:
-
 	/* 敵の生死 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Enemy)
-	bool IsAlive;
+	bool bIsAlive;
 
 	/* タイマーハンドル */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Enemy)
-	FTimerHandle TH_TargetLost;
+	FTimerHandle TimerHandle_TargetLost;
 
 	/* 現在の敵の体力 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Enemy)
@@ -106,4 +93,43 @@ protected:
 	/* 敵が死んで倒れるまでの時間 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Enemy)
 	float InLifeSpan;
+
+public:
+	int index = 0;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/* 被ダメージ処理関数 */
+	UFUNCTION()
+	void HandleAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
+
+public:
+	// Sets default values for this character's properties
+	AAIEnemy();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** キャラクターを攻撃する関数 */
+	void AttackCharacter_Implementation() override;
+
+	void CreateAmmunition();
+
+	/** キャラクターを追いかけるスピードを変更する関数 */
+	void ChangeRunningSpeed_Implementation(float Speed) override;
+
+	UFUNCTION()
+	void TimeLineFunc(float Value);
+
+	bool GetIsAlive() const { return bIsAlive; }
+
+	/* 現在の敵の体力のgetter */
+	UFUNCTION(BlueprintCallable, Category = Enemy)
+	float GetCurrentHealth() const;
+
+	/* 敵の体力の割合を返す関数 */
+	UFUNCTION(BlueprintCallable, Category = Enemy)
+	float GetHealthPercent() const;
 };
