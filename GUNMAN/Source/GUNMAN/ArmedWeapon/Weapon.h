67f594408c9a550db61fe80fe209a2986094b55a
yu03040 <kazumbl1no@gmail.com>
2024-08-20 18:19:21 +0900

    Fix：武器の説明をそれぞれのマークダウンに分ける

--- GUNMAN/Source/GUNMAN/ArmedWeapon/Weapon.h
+++ GUNMAN/Source/GUNMAN/ArmedWeapon/Weapon.h
@@ -29,9 +29,4 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };
